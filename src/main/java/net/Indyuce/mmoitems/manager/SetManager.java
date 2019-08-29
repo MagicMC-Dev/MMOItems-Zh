6443f85b4b3a9d8f57acaf7b614d81eb06c8624d
Indyuce <cym.peyrat@yahoo.fr>
2019-08-29 16:46:30 +0200

    add error handle for set manager

--- src/main/java/net/Indyuce/mmoitems/manager/SetManager.java
+++ src/main/java/net/Indyuce/mmoitems/manager/SetManager.java
@@ -3,12 +3,14 @@ package net.Indyuce.mmoitems.manager;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
+import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.ConfigFile;
 import net.Indyuce.mmoitems.api.ItemSet;
 
 public class SetManager {
-	private Map<String, ItemSet> itemSets = new HashMap<>();
+	private final Map<String, ItemSet> itemSets = new HashMap<>();
 
 	public SetManager() {
 		reload();
@@ -17,10 +19,13 @@ public class SetManager {
 	public void reload() {
 		ConfigFile config = new ConfigFile("item-sets");
 
-		// reload item sets and cache them into a map
 		itemSets.clear();
 		for (String id : config.getConfig().getKeys(false))
-			itemSets.put(id, new ItemSet(config.getConfig().getConfigurationSection(id)));
+			try {
+				itemSets.put(id, new ItemSet(config.getConfig().getConfigurationSection(id)));
+			} catch (IllegalArgumentException exception) {
+				MMOItems.plugin.getLogger().log(Level.WARNING, "Could not load item set '" + id + "': " + exception.getMessage());
+			}
 	}
 
 	public Collection<ItemSet> getAll() {

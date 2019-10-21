4187c19f997a37dc4f1ab8f138ad27d2caf1c21c
Indyuce <cym.peyrat@yahoo.fr>
2019-10-21 12:30:53 +0200

    !Java 8 format

--- src/main/java/net/Indyuce/mmoitems/comp/MMOItemsMetrics.java
+++ src/main/java/net/Indyuce/mmoitems/comp/MMOItemsMetrics.java
@@ -1,21 +1,16 @@
 package net.Indyuce.mmoitems.comp;
 
-import java.util.concurrent.Callable;
-
 import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.Type;
 
 public class MMOItemsMetrics extends Metrics {
 	public MMOItemsMetrics() {
 		super(MMOItems.plugin);
-		addCustomChart(new Metrics.SingleLineChart("items", new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				int total = 0;
-				for (Type type : MMOItems.plugin.getTypes().getAll())
-					total += type.getConfigFile().getConfig().getKeys(false).size();
-				return total;
-			}
+		addCustomChart(new Metrics.SingleLineChart("items", () -> {
+			int total = 0;
+			for (Type type : MMOItems.plugin.getTypes().getAll())
+				total += type.getConfigFile().getConfig().getKeys(false).size();
+			return total;
 		}));
 	}
 }

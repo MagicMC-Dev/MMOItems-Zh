2d8729daedd780a027a4f5faa5e76d9867a2a4e0
Aria Sangarin <ariahyrule@gmail.com>
2020-01-28 13:40:29 +0100

    Changed long lore line in custom sound gui

--- src/main/java/net/Indyuce/mmoitems/api/CustomSound.java
+++ src/main/java/net/Indyuce/mmoitems/api/CustomSound.java
@@ -12,7 +12,7 @@ public enum CustomSound {
 	ON_BLOCK_BREAK(Material.COBBLESTONE, 25, "Plays when a block is broken with the item."),
 	ON_PICKUP(Material.IRON_INGOT, 28, "Plays when you pickup the item from the ground."),
 	ON_LEFT_CLICK(Material.STONE_AXE, 31, "Plays when item is left-clicked."),
-	ON_CRAFT(VersionMaterial.CRAFTING_TABLE.toMaterial(), 34, "Plays when item is crafted in a crafting inventory,+or when smelted from someting in a furnace."),
+	ON_CRAFT(VersionMaterial.CRAFTING_TABLE.toMaterial(), 34, "Plays when item is crafted in a crafting inventory", "or when smelted from someting in a furnace."),
 	ON_CONSUME(Material.APPLE, 37, "Plays when item has been consumed.", "(After eating/drinking animation)"),
 	ON_ITEM_BREAK(Material.FLINT, 40, "Plays when the item breaks."),
 	ON_PLACED(Material.STONE, 43, "Plays when the block is placed.");

a94db8cf701cc356a45fed680095508c6bc312f8
Ashijin <xikage@gmail.com>
2020-02-17 20:38:01 -0700

    Added some stuff to the durability API

--- src/main/java/net/Indyuce/mmoitems/listener/NewDurabilityListener.java
+++ src/main/java/net/Indyuce/mmoitems/listener/NewDurabilityListener.java
@@ -5,7 +5,9 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerItemMendEvent;
+import org.bukkit.plugin.RegisteredListener;
 
+import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.interaction.util.DurabilityItem;
 
 public class NewDurabilityListener implements Listener {
@@ -13,8 +15,8 @@ public class NewDurabilityListener implements Listener {
 	@EventHandler(ignoreCancelled = true)
 	public void a(PlayerItemDamageEvent event) {
 		DurabilityItem item = new DurabilityItem(event.getPlayer(), event.getItem());
-		if (item.isValid()) {
 
+		if (item.isValid()) {
 			/*
 			 * calculate item durability loss
 			 */
@@ -25,7 +27,9 @@ public class NewDurabilityListener implements Listener {
 			 * do NOT cancel the event and make sure the item is destroyed
 			 */
 			if (item.isBroken() && item.isLostWhenBroken()) {
+				event.getItem().setDurability(event.getItem().getType().getMaxDurability());
 				event.setDamage(999);
+
 				return;
 			}
 

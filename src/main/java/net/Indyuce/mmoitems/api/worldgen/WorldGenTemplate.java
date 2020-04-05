997df8174318e5f96462d75ff25474d6e3a15f2d
Indyuce <cym.peyrat@yahoo.fr>
2020-04-05 23:38:18 +0200

    !World gen cleanup, improved default configs

--- src/main/java/net/Indyuce/mmoitems/api/worldgen/WorldGenTemplate.java
+++ src/main/java/net/Indyuce/mmoitems/api/worldgen/WorldGenTemplate.java
@@ -3,40 +3,38 @@ package net.Indyuce.mmoitems.api.worldgen;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Biome;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class WorldGenTemplate {
-	public double chunkChance;
-	public int minDepth, maxDepth, veinSize, veinCount;
+	public final double chunkChance;
+	public final int minDepth, maxDepth, veinSize, veinCount;
 
-	private List<Material> replaceableMaterials = new ArrayList<>();
-	private List<String> worldWhitelist = new ArrayList<>(), worldBlacklist = new ArrayList<>();
-	private List<String> biomeWhitelist = new ArrayList<>(), biomeBlacklist = new ArrayList<>();
-	private boolean slimeChunk;
+	private final List<Material> replaceable = new ArrayList<>();
+	private final List<String> worldWhitelist = new ArrayList<>(), worldBlacklist = new ArrayList<>();
+	private final List<String> biomeWhitelist = new ArrayList<>(), biomeBlacklist = new ArrayList<>();
+	private final boolean slimeChunk;
 
 	public WorldGenTemplate(ConfigurationSection config) {
-		List<String> matList = config.getStringList("replace");
-		for (String material : matList)
-			replaceableMaterials.add(Material.valueOf(material));
+		Validate.notNull(config, "Could not read gen template config");
 
-		List<String> worldList = config.getStringList("worlds");
-		List<String> biomeList = config.getStringList("biomes");
+		config.getStringList("replace").forEach(str -> replaceable.add(Material.valueOf(str.toUpperCase().replace("-", "_").replace(" ", "_"))));
 
-		for (String world : worldList)
+		for (String world : config.getStringList("worlds"))
 			if (world.contains("!"))
 				worldBlacklist.add(world.toUpperCase());
 			else
 				worldWhitelist.add(world.toUpperCase());
 
-		for (String biome : biomeList)
+		for (String biome : config.getStringList("biomes"))
 			if (biome.contains("!"))
 				biomeBlacklist.add(biome.toUpperCase());
 			else
 				biomeWhitelist.add(biome.toUpperCase());
-
+		
 		chunkChance = config.getDouble("chunk-chance");
 		slimeChunk = config.getBoolean("slime-chunk", false);
 
@@ -49,22 +47,29 @@ public class WorldGenTemplate {
 	}
 
 	public boolean canGenerate(Location pos) {
+
+		// check world list
 		if (!worldWhitelist.isEmpty() && !worldWhitelist.contains(pos.getWorld().getName().toUpperCase()))
 			return false;
 		if (!worldBlacklist.isEmpty() && worldBlacklist.contains(pos.getWorld().getName().toUpperCase()))
 			return false;
+
+		// check biome list
 		Biome biome = pos.getWorld().getBiome(pos.getBlockX(), pos.getBlockZ());
 		if (!biomeWhitelist.isEmpty() && !biomeWhitelist.contains(biome.name()))
 			return false;
 		if (!biomeBlacklist.isEmpty() && biomeBlacklist.contains(biome.name()))
 			return false;
+
+		// check extra options
 		if (slimeChunk && !pos.getChunk().isSlimeChunk())
 			return false;
 
+		// can generate if no restrictions applied
 		return true;
 	}
 
 	public boolean canReplace(Material type) {
-		return replaceableMaterials.isEmpty() || replaceableMaterials.contains(type);
+		return replaceable.isEmpty() || replaceable.contains(type);
 	}
 }
--- src/main/java/net/Indyuce/mmoitems/manager/WorldGenManager.java
+++ src/main/java/net/Indyuce/mmoitems/manager/WorldGenManager.java
@@ -19,53 +19,51 @@ import net.Indyuce.mmoitems.api.CustomBlock;
 import net.Indyuce.mmoitems.api.worldgen.WorldGenTemplate;
 
 public class WorldGenManager implements Listener {
-	Map<String, WorldGenTemplate> templates = new HashMap<String, WorldGenTemplate>();
-	Map<Integer, String> assigned = new HashMap<Integer, String>();
-	BlockFace[] faces = { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.WEST, BlockFace.EAST, BlockFace.DOWN, BlockFace.UP };
-	Random rnd = new Random();
-	
+	private final Map<String, WorldGenTemplate> templates = new HashMap<>();
+	private final Map<Integer, String> assigned = new HashMap<>();
+
+	private static final BlockFace[] faces = { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.WEST, BlockFace.EAST, BlockFace.DOWN, BlockFace.UP };
+	private static final Random random = new Random();
+
 	public WorldGenManager() {
 		FileConfiguration config = new ConfigFile("gen-templates").getConfig();
-		
-		config.getKeys(false).forEach(e -> {
-			MMOItems.plugin.getLogger().info("WorldGenTemplate: " + e);
-			templates.put(e, new WorldGenTemplate(config.getConfigurationSection(e)));
-		});
-		
-		if(MMOItems.plugin.getLanguage().worldGenEnabled) Bukkit.getPluginManager().registerEvents(this, MMOItems.plugin);
+		config.getKeys(false).forEach(e -> templates.put(e, new WorldGenTemplate(config.getConfigurationSection(e))));
+
+		if (MMOItems.plugin.getLanguage().worldGenEnabled)
+			Bukkit.getPluginManager().registerEvents(this, MMOItems.plugin);
 	}
-	
+
+	public void register(CustomBlock block) {
+		assigned.put(block.getId(), block.getTemplateName());
+	}
+
 	@EventHandler
 	public void a(ChunkLoadEvent event) {
-		if(!event.isNewChunk()) return;
-		
-		for(int blocks : assigned.keySet()) {
+		if (!event.isNewChunk())
+			return;
+
+		for (int blocks : assigned.keySet()) {
 			WorldGenTemplate wgt = templates.get(assigned.get(blocks));
 
-			if(rnd.nextDouble() < wgt.chunkChance) {
-				for(int i = 0; i < wgt.veinCount; i++) {
-					int y = rnd.nextInt((wgt.maxDepth - wgt.minDepth) + 1) + wgt.minDepth;
-					Location generatePoint = event.getChunk().getBlock(rnd.nextInt(16), y, rnd.nextInt(16)).getLocation();
-					if(wgt.canGenerate(generatePoint)) {
+			if (random.nextDouble() < wgt.chunkChance)
+				for (int i = 0; i < wgt.veinCount; i++) {
+					int y = random.nextInt((wgt.maxDepth - wgt.minDepth) + 1) + wgt.minDepth;
+					Location generatePoint = event.getChunk().getBlock(random.nextInt(16), y, random.nextInt(16)).getLocation();
+					if (wgt.canGenerate(generatePoint)) {
 						CustomBlock block = MMOItems.plugin.getCustomBlocks().getBlock(blocks);
 						Block modify = event.getWorld().getBlockAt(generatePoint);
 
-						for(int j = 0; j < wgt.veinSize; j++) {
-							if(wgt.canReplace(modify.getType())) {
+						for (int j = 0; j < wgt.veinSize; j++) {
+							if (wgt.canReplace(modify.getType())) {
 								modify.setType(block.getType(), false);
 								modify.setBlockData(block.getBlockData(), false);
 							}
 
-							BlockFace nextFace = faces[rnd.nextInt(faces.length)];
+							BlockFace nextFace = faces[random.nextInt(faces.length)];
 							modify = modify.getRelative(nextFace);
 						}
 					}
 				}
-			}
 		}
-	}
-	
-	public void register(CustomBlock block) {
-		assigned.put(block.getId(), block.getTemplateName());
 	}
 }

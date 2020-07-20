a1b4b1aff182ec0eca5517ce85f010dd686f59ac
Indyuce <cym.peyrat@yahoo.fr>
2020-07-20 17:52:00 +0200

    Small cleanup

--- src/main/java/net/Indyuce/mmoitems/api/edition/Edition.java
+++ src/main/java/net/Indyuce/mmoitems/api/edition/Edition.java
@@ -1,9 +1,18 @@
 package net.Indyuce.mmoitems.api.edition;
 
 public interface Edition {
+
+	/*
+	 * processes the player input; returns true if edition should be closed or
+	 * false if it should continue
+	 */
 	public boolean output(String input);
-	
+
 	public void enable(String... message);
-	
+
+	/*
+	 * true if after successful edition, the GUI should go back to the
+	 * previously opened GUI or if it should just be ignored
+	 */
 	public boolean shouldGoBack();
 }

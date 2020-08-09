2515e563a7496ae2218f31de55dba63cdd680af8
Ethan <ethan@plexiate.com>
2020-08-09 18:58:28 -0400

    fixed mm faction damage issue with older minecraft versions

--- src/main/java/net/Indyuce/mmoitems/comp/mythicmobs/stat/FactionDamage.java
+++ src/main/java/net/Indyuce/mmoitems/comp/mythicmobs/stat/FactionDamage.java
@@ -1,13 +1,12 @@
 package net.Indyuce.mmoitems.comp.mythicmobs.stat;
 
 import net.Indyuce.mmoitems.stat.type.DoubleStat;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import net.mmogroup.mmolib.version.VersionMaterial;
 
 public class FactionDamage extends DoubleStat {
 
     public FactionDamage(String faction) {
-        super("FACTION_DAMAGE_" + faction.toUpperCase(), new ItemStack(Material.RED_DYE), faction + " Faction Damage", new String[] { "Deals additional damage to mobs", "from the " + faction + " faction in %." }, new String[] { "!block", "all" });
+        super("FACTION_DAMAGE_" + faction.toUpperCase(), VersionMaterial.RED_DYE.toItem(), faction + " Faction Damage", new String[] { "Deals additional damage to mobs", "from the " + faction + " faction in %." }, new String[] { "!block", "all" });
     }
 }
 

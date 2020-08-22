b029c1efb6a7e556f8c8ff4e5c8b0c118c693873
Blekdigits <kelvinjunilson@gmail.com>
2020-08-22 21:04:06 +0700

    fix custom recipe not giving the result item

--- src/main/java/net/Indyuce/mmoitems/listener/CraftingListener.java
+++ src/main/java/net/Indyuce/mmoitems/listener/CraftingListener.java
@@ -73,7 +73,7 @@ public class CraftingListener implements Listener {
 						inv.setMatrix(newMatrix);
 				}
 			}, 1);
-			// e.setCurrentItem(cached.getResult());
+			e.setCurrentItem(cached.getResult());
 		}
 	}
 

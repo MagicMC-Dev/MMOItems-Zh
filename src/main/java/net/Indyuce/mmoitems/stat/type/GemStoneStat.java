bc38e9233a0f6daab40b1f7b242b9b8025767dc4
Ethan <ethan@plexiate.com>
2020-12-23 23:12:08 -0500

    !typo

--- src/main/java/net/Indyuce/mmoitems/stat/type/GemStoneStat.java
+++ src/main/java/net/Indyuce/mmoitems/stat/type/GemStoneStat.java
@@ -3,7 +3,7 @@ package net.Indyuce.mmoitems.stat.type;
 /**
  * Statistics which must NOT be applied onto an item when socketing the gem
  * stone. For instance, 'Success Rate' is a gem stone stat but it must not be
- * transfered onto the item.
+ * transferred onto the item.
  * 
  * @author cympe
  */

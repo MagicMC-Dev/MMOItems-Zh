73388799827e98cbfc413275c666436c4f3d18c6
ASangarin <ariasangarin@gmail.com>
2021-01-02 07:21:16 +0100

    Any item is now compatible with custom durability if put in an armor slot (somehow)

--- src/main/java/net/Indyuce/mmoitems/listener/DurabilityListener.java
+++ src/main/java/net/Indyuce/mmoitems/listener/DurabilityListener.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +14,7 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerItemMendEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 import net.Indyuce.mmoitems.api.interaction.util.DurabilityItem;
@@ -20,9 +22,10 @@ import net.Indyuce.mmoitems.api.interaction.util.DurabilityItem;
 public class DurabilityListener implements Listener {
 	private final List<DamageCause> ignoredCauses = Arrays.asList(DamageCause.DROWNING, DamageCause.SUICIDE, DamageCause.FALL, DamageCause.VOID,
 			DamageCause.FIRE_TICK, DamageCause.SUFFOCATION, DamageCause.POISON, DamageCause.WITHER, DamageCause.STARVATION, DamageCause.MAGIC);
+	private final EquipmentSlot[] slots = { EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET };
 
 	/**
-	 * Handles custom durability for player heads
+	 * Handles custom durability for non-'vanilla durability' items
 	 */
 	@EventHandler(ignoreCancelled = true)
 	public void playerDamage(EntityDamageEvent event) {
@@ -30,33 +33,10 @@ public class DurabilityListener implements Listener {
 			return;
 
 		Player player = (Player) event.getEntity();
-		if (player.getEquipment().getHelmet() == null || player.getEquipment().getHelmet().getType() != Material.PLAYER_HEAD)
-			return;
-
-		ItemStack helmet = player.getEquipment().getHelmet();
-		DurabilityItem item = new DurabilityItem(player, helmet);
-
-		if (item.isValid()) {
-			/*
-			 * Calculate item durability loss
-			 *
-			 * This uses the vanilla formula of 1 durability per 4 damage.
-			 * (rounded down, but never below 1)
-			 */
-			item.decreaseDurability(Math.max((int) event.getDamage() / 4, 1));
-
-			/*
-			 * If the item is broken and if it is meant to be lost when broken,
-			 * do NOT cancel the event and make sure the item is destroyed
-			 */
-			if (item.isBroken() && item.isLostWhenBroken()) {
-				player.getEquipment().setHelmet(null);
-				return;
-			}
-
-			helmet.setItemMeta(item.toItem().getItemMeta());
-			player.getEquipment().setHelmet(helmet);
-		}
+		int damage = Math.max((int) event.getDamage() / 4, 1);
+		for(EquipmentSlot slot : slots)
+			if(hasItem(player, slot))
+				handleVanillaDamage(player.getInventory().getItem(slot), player, slot, damage);
 	}
 
 	@EventHandler(ignoreCancelled = true)
@@ -88,5 +68,25 @@ public class DurabilityListener implements Listener {
 		DurabilityItem durItem = new DurabilityItem(event.getPlayer(), event.getItem());
 		if (durItem.isValid())
 			event.getItem().setItemMeta(durItem.addDurability(event.getRepairAmount()).toItem().getItemMeta());
+	}
+
+	private void handleVanillaDamage(ItemStack stack, Player player, EquipmentSlot slot, int damage) {
+		DurabilityItem item = new DurabilityItem(player, stack);
+
+		if (item.isValid()) {
+			item.decreaseDurability(damage);
+
+			if (item.isBroken() && item.isLostWhenBroken()) {
+				player.getInventory().setItem(slot, null);
+				player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0f, 1.0f);
+				return;
+			}
+
+			player.getInventory().getItem(slot).setItemMeta(item.toItem().getItemMeta());
+		}
+	}
+
+	private boolean hasItem(Player player, EquipmentSlot slot) {
+		return player.getInventory().getItem(slot) != null && player.getInventory().getItem(slot).getType() != Material.AIR;
 	}
 }

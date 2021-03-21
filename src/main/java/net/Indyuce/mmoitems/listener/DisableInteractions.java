daf1e1568033d690776f9b256c0e5f2dd6b32eba
Adam <kasprryt@gmail.com>
2021-03-20 20:03:04 -0400

    Broken weapons no longer do damage

--- src/main/java/net/Indyuce/mmoitems/listener/DisableInteractions.java
+++ src/main/java/net/Indyuce/mmoitems/listener/DisableInteractions.java
@@ -3,12 +3,16 @@ package net.Indyuce.mmoitems.listener;
 import net.Indyuce.mmoitems.MMOItems;
 import io.lumine.mythic.lib.MythicLib;
 import io.lumine.mythic.lib.api.item.NBTItem;
+import net.Indyuce.mmoitems.api.interaction.util.DurabilityItem;
 import org.bukkit.Keyed;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.FurnaceSmeltEvent;
@@ -140,6 +144,11 @@ public class DisableInteractions implements Listener {
 		if (!(event.getEntity() instanceof Player))
 			return;
 
+		DurabilityItem durItem = new DurabilityItem(((Player) event.getEntity()).getPlayer(), event.getBow());
+
+		if (durItem.isBroken())
+			event.setCancelled(true);
+
 		Player player = (Player) event.getEntity();
 		int arrowSlot = firstArrow(player);
 		if (arrowSlot < 0)
@@ -174,5 +183,20 @@ public class DisableInteractions implements Listener {
 	private boolean isDisabled(NBTItem nbt, String type) {
 		return nbt.hasType() && MMOItems.plugin.getConfig().getBoolean("disable-interactions." + type)
 				|| nbt.getBoolean("MMOITEMS_DISABLE_" + type.toUpperCase().replace("-", "_") + "ING");
+	}
+
+	// If weapon is broken don't do damage
+	@EventHandler
+	public void playerAttack(EntityDamageByEntityEvent event) {
+		if (event.getDamage() == 0 || event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK || !(event.getEntity() instanceof LivingEntity)
+						|| !(event.getDamager() instanceof Player) || event.getEntity().hasMetadata("NPC") || event.getDamager().hasMetadata("NPC"))
+			return;
+		Player player = (Player) event.getDamager();
+		ItemStack item = player.getInventory().getItemInMainHand();
+
+		DurabilityItem durItem = new DurabilityItem(player, item);
+
+		if (durItem.isBroken())
+			event.setCancelled(true);
 	}
 }

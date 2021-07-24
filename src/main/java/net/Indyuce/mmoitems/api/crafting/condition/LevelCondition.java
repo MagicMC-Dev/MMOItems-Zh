2354211e5ca125c33f6e188ac8e22e85f49e7246
Jules <peyrat.jules@yahoo.fr>
2021-07-24 15:59:20 +0200

    New option to consume vanilla levels on recipe

--- src/main/java/net/Indyuce/mmoitems/api/crafting/condition/LevelCondition.java
+++ src/main/java/net/Indyuce/mmoitems/api/crafting/condition/LevelCondition.java
@@ -1,29 +1,38 @@
 package net.Indyuce.mmoitems.api.crafting.condition;
 
-import net.Indyuce.mmoitems.api.player.PlayerData;
 import io.lumine.mythic.lib.api.MMOLineConfig;
+import net.Indyuce.mmoitems.api.player.PlayerData;
 
 public class LevelCondition extends Condition {
-	private final int level;
+    private final int level;
 
-	public LevelCondition(MMOLineConfig config) {
-		super("level");
+    /**
+     * Should this amount of vanilla levels be
+     * consumed when the crafting recipe is used
+     */
+    private final boolean consume;
 
-		config.validate("level");
-		level = config.getInt("level");
-	}
+    public LevelCondition(MMOLineConfig config) {
+        super("level");
 
-	@Override
-	public boolean isMet(PlayerData data) {
-		return data.getRPG().getLevel() >= level;
-	}
+        config.validate("level");
+        level = config.getInt("level");
+        consume = config.getBoolean("consume", false);
+    }
 
-	@Override
-	public String formatDisplay(String string) {
-		return string.replace("#level#", "" + level);
-	}
+    @Override
+    public boolean isMet(PlayerData data) {
+        return data.getRPG().getLevel() >= level;
+    }
 
-	@Override
-	public void whenCrafting(PlayerData data) {
-	}
+    @Override
+    public String formatDisplay(String string) {
+        return string.replace("#level#", "" + level);
+    }
+
+    @Override
+    public void whenCrafting(PlayerData data) {
+        if (consume)
+            data.getPlayer().setLevel(Math.max(0, data.getPlayer().getLevel() - level));
+    }
 }

4b0b41cc307197f9c3e5902146261ce78427f1dd
Jules <peyrat.jules@yahoo.fr>
2021-07-26 12:12:51 +0200

    !Fixed comp with Skills(Pro)

--- src/main/java/net/Indyuce/mmoitems/comp/rpg/SkillsHook.java
+++ src/main/java/net/Indyuce/mmoitems/comp/rpg/SkillsHook.java
@@ -1,71 +1,70 @@
 package net.Indyuce.mmoitems.comp.rpg;
 
-import org.bukkit.OfflinePlayer;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
 import me.leothepro555.skills.database.managers.PlayerInfo;
 import me.leothepro555.skills.events.SkillLevelUpEvent;
 import me.leothepro555.skills.main.Skills;
 import me.leothepro555.skilltype.ScalingType;
 import net.Indyuce.mmoitems.api.player.PlayerData;
 import net.Indyuce.mmoitems.api.player.RPGPlayer;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
 public class SkillsHook implements RPGHandler, Listener {
 
-	@EventHandler
-	public void a(SkillLevelUpEvent event) {
-		OfflinePlayer player = event.getPlayer();
-		if (player.isOnline())
-			PlayerData.get(player).getInventory().scheduleUpdate();
-	}
+    @EventHandler
+    public void a(SkillLevelUpEvent event) {
+        OfflinePlayer player = event.getPlayer();
+        if (player.isOnline())
+            PlayerData.get(player).getInventory().scheduleUpdate();
+    }
 
-	@Override
-	public void refreshStats(PlayerData data) {
-	}
+    @Override
+    public void refreshStats(PlayerData data) {
+    }
 
-	@Override
-	public RPGPlayer getInfo(PlayerData data) {
-		return new SkillsPlayer(data);
-	}
+    @Override
+    public RPGPlayer getInfo(PlayerData data) {
+        return new SkillsPlayer(data);
+    }
 
-	public static class SkillsPlayer extends RPGPlayer {
-		private final PlayerInfo info;
+    public static class SkillsPlayer extends RPGPlayer {
+        public SkillsPlayer(PlayerData playerData) {
+            super(playerData);
+        }
 
-		public SkillsPlayer(PlayerData playerData) {
-			super(playerData);
+        @Override
+        public int getLevel() {
+            PlayerInfo info = Skills.get().getPlayerDataManager().loadPlayerInfo(getPlayer());
+            return info.getLevel();
+        }
 
-			info = Skills.get().getPlayerDataManager().loadPlayerInfo(playerData.getPlayer());
-		}
+        @Override
+        public String getClassName() {
+            PlayerInfo info = Skills.get().getPlayerDataManager().loadPlayerInfo(getPlayer());
+            return info.getSkill().getLanguageName().getDefault();
+        }
 
-		@Override
-		public int getLevel() {
-			return info.getLevel();
-		}
+        @Override
+        public double getMana() {
+            PlayerInfo info = Skills.get().getPlayerDataManager().loadPlayerInfo(getPlayer());
+            return info.getActiveStatType(ScalingType.ENERGY);
+        }
 
-		@Override
-		public String getClassName() {
-			return info.getSkill().getLanguageName().getDefault();
-		}
+        @Override
+        public double getStamina() {
+            return getPlayer().getFoodLevel();
+        }
 
-		@Override
-		public double getMana() {
-			return info.getActiveStatType(ScalingType.ENERGY);
-		}
+        @Override
+        public void setMana(double value) {
+            PlayerInfo info = Skills.get().getPlayerDataManager().loadPlayerInfo(getPlayer());
+            info.setActiveStatType(ScalingType.ENERGY, value);
+        }
 
-		@Override
-		public double getStamina() {
-			return getPlayer().getFoodLevel();
-		}
-
-		@Override
-		public void setMana(double value) {
-			info.setActiveStatType(ScalingType.ENERGY, value);
-		}
-
-		@Override
-		public void setStamina(double value) {
-			getPlayer().setFoodLevel((int) value);
-		}
-	}
+        @Override
+        public void setStamina(double value) {
+            getPlayer().setFoodLevel((int) value);
+        }
+    }
 }
--- src/main/java/net/Indyuce/mmoitems/comp/rpg/SkillsProHook.java
+++ src/main/java/net/Indyuce/mmoitems/comp/rpg/SkillsProHook.java
@@ -1,71 +1,70 @@
 package net.Indyuce.mmoitems.comp.rpg;
 
+import net.Indyuce.mmoitems.api.player.PlayerData;
+import net.Indyuce.mmoitems.api.player.RPGPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import net.Indyuce.mmoitems.api.player.PlayerData;
-import net.Indyuce.mmoitems.api.player.RPGPlayer;
 import org.skills.api.events.SkillLevelUpEvent;
 import org.skills.data.managers.SkilledPlayer;
 import org.skills.main.SkillsPro;
 
 public class SkillsProHook implements RPGHandler, Listener {
 
-	@EventHandler
-	public void a(SkillLevelUpEvent event) {
-		OfflinePlayer player = event.getPlayer();
-		if (player.isOnline())
-			PlayerData.get(player).getInventory().scheduleUpdate();
-	}
+    @EventHandler
+    public void a(SkillLevelUpEvent event) {
+        OfflinePlayer player = event.getPlayer();
+        if (player.isOnline())
+            PlayerData.get(player).getInventory().scheduleUpdate();
+    }
 
-	@Override
-	public void refreshStats(PlayerData data) {
-	}
+    @Override
+    public void refreshStats(PlayerData data) {
+    }
 
-	@Override
-	public RPGPlayer getInfo(PlayerData data) {
-		return new SkillsPlayer(data);
-	}
+    @Override
+    public RPGPlayer getInfo(PlayerData data) {
+        return new SkillsPlayer(data);
+    }
 
-	public static class SkillsPlayer extends RPGPlayer {
-		private final SkilledPlayer info;
+    public static class SkillsPlayer extends RPGPlayer {
+        public SkillsPlayer(PlayerData playerData) {
+            super(playerData);
+        }
 
-		public SkillsPlayer(PlayerData playerData) {
-			super(playerData);
+        @Override
+        public int getLevel() {
+            SkilledPlayer info = SkillsPro.get().getPlayerDataManager().getData(getPlayerData().getUniqueId());
+            return info.getLevel();
+        }
 
-			info = SkillsPro.get().getPlayerDataManager().getData(playerData.getUniqueId());
-		}
+        @Override
+        public String getClassName() {
+            SkilledPlayer info = SkillsPro.get().getPlayerDataManager().getData(getPlayerData().getUniqueId());
+            return ChatColor.stripColor(info.getSkill().getDisplayName());
+        }
 
-		@Override
-		public int getLevel() {
-			return info.getLevel();
-		}
+        @Override
+        public double getMana() {
+            SkilledPlayer info = SkillsPro.get().getPlayerDataManager().getData(getPlayerData().getUniqueId());
+            return info.getEnergy();
+        }
 
-		@Override
-		public String getClassName() {
-			return ChatColor.stripColor(info.getSkill().getDisplayName());
-		}
+        @Override
+        public double getStamina() {
+            return getPlayer().getFoodLevel();
+        }
 
-		@Override
-		public double getMana() {
-			return info.getEnergy();
-		}
+        @Override
+        public void setMana(double value) {
+            SkilledPlayer info = SkillsPro.get().getPlayerDataManager().getData(getPlayerData().getUniqueId());
+            info.setEnergy(value);
+        }
 
-		@Override
-		public double getStamina() {
-			return getPlayer().getFoodLevel();
-		}
-
-		@Override
-		public void setMana(double value) {
-			info.setEnergy(value);
-		}
-
-		@Override
-		public void setStamina(double value) {
-			getPlayer().setFoodLevel((int) value);
-		}
-	}
+        @Override
+        public void setStamina(double value) {
+            getPlayer().setFoodLevel((int) value);
+        }
+    }
 }

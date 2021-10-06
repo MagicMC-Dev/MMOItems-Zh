c28107a74399714ec4890c07ba387a289640cefe
Dancull47 <Dancull47@gmail.com>
2021-10-06 15:39:28 -0400

    - Fixed error with Math for newer Java versions - Adjusted syntax to be more compatible with equations
    New syntax is `%`s instead of `()`s
    For example `movement-speed: 'Speed: +MATH%(#+10)*10%'`

--- src/main/java/net/Indyuce/mmoitems/api/item/build/LoreBuilder.java
+++ src/main/java/net/Indyuce/mmoitems/api/item/build/LoreBuilder.java
@@ -160,17 +160,9 @@ public class LoreBuilder {
          * */
         int index = 0;
         for (String string : lore) {
-            if (string.contains("MATH(")) {
-                ScriptEngineManager manager = new ScriptEngineManager();
-                ScriptEngine engine = manager.getEngineByName("js");
-                String result = "MATH ERROR";
-                try {
-                    result = String.valueOf(engine.eval(StringUtils.substringBetween(string, "(", ")")));
-                } catch (ScriptException e) {
-                    MMOItems.plugin.getLogger().log(Level.WARNING, FriendlyFeedbackProvider.quickForConsole(FFPMMOItems.get(),
-                            "An Item Stat which uses MATH is incorrectly setup. Check your stats.yml file and verify any MATH(X) usage is a valid equation!"));
-                }
-                lore.set(index, string.replaceAll("MATH\\([^)]*\\)", result));
+            if (string.contains("MATH%")) {
+                String result = String.valueOf(eval(StringUtils.substringBetween(string, "%", "%")));
+                lore.set(index, string.replaceAll("MATH\\%[^%]*\\%", result));
             }
             index++;
 
@@ -212,4 +204,91 @@ public class LoreBuilder {
     private boolean isBar(String str) {
         return str.startsWith("{bar}") || str.startsWith("{sbar}");
     }
+
+    /*
+     *
+     * Math methods
+     *
+     * */
+
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
 }

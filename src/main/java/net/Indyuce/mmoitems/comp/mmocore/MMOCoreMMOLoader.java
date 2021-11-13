9d379b6674470af8b9bac6b47633866a727a54d9
Indyuce <cym.peyrat@yahoo.fr>
2021-11-13 19:24:26 +0100

    New stats for MMOCore resource regen

--- src/main/java/net/Indyuce/mmoitems/comp/mmocore/MMOCoreMMOLoader.java
+++ src/main/java/net/Indyuce/mmoitems/comp/mmocore/MMOCoreMMOLoader.java
@@ -29,19 +29,22 @@ import java.util.Optional;
 
 public class MMOCoreMMOLoader extends MMOLoader {
 
-    private static final ItemStat MANA_REGENERATION = new DoubleStat("MANA_REGENERATION", VersionMaterial.LAPIS_LAZULI.toMaterial(), "Mana Regeneration",
-            new String[]{"Increases mana regen."});
+    private static final ItemStat HEALTH_REGENERATION = new DoubleStat("HEALTH_REGENERATION", Material.BREAD, "Health Regeneration", new String[]{"Amount of health pts regenerated every second."});
+    private static final ItemStat MAX_HEALTH_REGENERATION = new DoubleStat("MAX_HEALTH_REGENERATION", Material.BREAD, "Max Health Regeneration", new String[]{"Percentage of max health regenerated every second."});
+    private static final ItemStat MANA_REGENERATION = new DoubleStat("MANA_REGENERATION", VersionMaterial.LAPIS_LAZULI.toMaterial(), "Mana Regeneration", new String[]{"Amount of mana pts regenerated every second."});
+    private static final ItemStat MAX_MANA_REGENERATION = new DoubleStat("MAX_MANA_REGENERATION", VersionMaterial.LAPIS_LAZULI.toMaterial(), "Max Mana Regeneration", new String[]{"Percentage of max mana regenerated every second."});
+    private static final ItemStat STAMINA_REGENERATION = new DoubleStat("STAMINA_REGENERATION", VersionMaterial.LIGHT_BLUE_DYE.toMaterial(), "Stamina Regeneration", new String[]{"Amount of stamina pts regenerated every second."});
+    private static final ItemStat MAX_STAMINA_REGENERATION = new DoubleStat("MAX_STAMINA_REGENERATION", VersionMaterial.LIGHT_BLUE_DYE.toMaterial(), "Max Stamina Regeneration", new String[]{"Percentage of max stamina regenerated every second."});
+
     private static final ItemStat MAX_STAMINA = new DoubleStat("MAX_STAMINA", VersionMaterial.LIGHT_BLUE_DYE.toMaterial(), "Max Stamina",
             new String[]{"Adds stamina to your max stamina bar."});
-    private static final ItemStat STAMINA_REGENERATION = new DoubleStat("STAMINA_REGENERATION", VersionMaterial.LIGHT_BLUE_DYE.toMaterial(),
-            "Stamina Regeneration", new String[]{"Increases stamina regen."});
+    private static final ItemStat MAX_STELLIUM = new DoubleStat("MAX_STELLIUM", VersionMaterial.ENDER_EYE.toMaterial(), "Max Stellium",
+            new String[]{"Additional maximum stellium."});
     private static final ItemStat ADDITIONAL_EXPERIENCE = new DoubleStat("ADDITIONAL_EXPERIENCE", VersionMaterial.EXPERIENCE_BOTTLE.toMaterial(),
             "Additional Experience", new String[]{"Additional MMOCore main class experience in %."});
-    private static final ItemStat HEALTH_REGENERATION = new DoubleStat("HEALTH_REGENERATION", Material.BREAD, "Health Regeneration",
-            new String[]{"Increases MMOCore health regen.", "In %."});
 
     /*
-     * called when MMOItems loads
+     * Called when MMOItems loads
      */
     public MMOCoreMMOLoader() {
         MMOCore.plugin.loadManager.registerLoader(this);
@@ -51,9 +54,13 @@ public class MMOCoreMMOLoader extends MMOLoader {
         });
 
         MMOItems.plugin.getStats().register(HEALTH_REGENERATION);
+        MMOItems.plugin.getStats().register(MAX_HEALTH_REGENERATION);
         MMOItems.plugin.getStats().register(MANA_REGENERATION);
-        MMOItems.plugin.getStats().register(MAX_STAMINA);
+        MMOItems.plugin.getStats().register(MAX_MANA_REGENERATION);
         MMOItems.plugin.getStats().register(STAMINA_REGENERATION);
+        MMOItems.plugin.getStats().register(MAX_STAMINA_REGENERATION);
+        MMOItems.plugin.getStats().register(MAX_STAMINA);
+        MMOItems.plugin.getStats().register(MAX_STELLIUM);
         MMOItems.plugin.getStats().register(ADDITIONAL_EXPERIENCE);
 
         /*

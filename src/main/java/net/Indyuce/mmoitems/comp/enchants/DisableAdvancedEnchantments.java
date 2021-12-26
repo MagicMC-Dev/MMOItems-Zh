98b626bfb8fc7b8ba6d1205066e141c9d0c05489
Indyuce <cym.peyrat@yahoo.fr>
2021-12-26 23:42:46 +0100

    build now

--- src/main/java/net/Indyuce/mmoitems/comp/enchants/DisableAdvancedEnchantments.java
+++ src/main/java/net/Indyuce/mmoitems/comp/enchants/DisableAdvancedEnchantments.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 
 public class DisableAdvancedEnchantments extends DisableStat {
     public DisableAdvancedEnchantments() {
+
         super("ADVANCED_ENCHANTS", Material.ENCHANTED_BOOK, "Disable Advanced Enchants", new String[]{"all"}, "When toggled on, prevents players", "from applying AE onto this item.");
     }
 }

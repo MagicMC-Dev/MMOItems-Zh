09d71f46ae1fc5228c9637b328a6d54c68c0f479
Indyuce <cym.peyrat@yahoo.fr>
2022-02-27 13:36:53 +0100

    improved placeholder condition

--- src/main/java/net/Indyuce/mmoitems/api/crafting/condition/PlaceholderCondition.java
+++ src/main/java/net/Indyuce/mmoitems/api/crafting/condition/PlaceholderCondition.java
@@ -3,16 +3,17 @@ package net.Indyuce.mmoitems.api.crafting.condition;
 import io.lumine.mythic.lib.api.MMOLineConfig;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.Indyuce.mmoitems.api.player.PlayerData;
+import org.apache.commons.lang.Validate;
 
 public class PlaceholderCondition extends GenericCondition {
-    private final String value, placeholder, comparator, compareTo;
+    private final String placeholder, comparator, compareTo;
 
     public PlaceholderCondition(MMOLineConfig config) {
         super("placeholder", config);
 
         config.validate("placeholder");
-        value = config.getString("placeholder");
-        String[] array = value.split("~");
+        String[] array = config.getString("placeholder").split("~");
+        Validate.isTrue(array.length == 3, "Please use exactly three times ~");
         placeholder = array[0];
         comparator = array[1];
         compareTo = array[2];
@@ -24,16 +25,16 @@ public class PlaceholderCondition extends GenericCondition {
         switch (comparator) {
             case "<":
                 return Double.valueOf(placeholders) < Double.valueOf(compareTo);
-            case "<+":
+            case "<=":
                 return Double.valueOf(placeholders) <= Double.valueOf(compareTo);
             case ">":
                 return Double.valueOf(placeholders) > Double.valueOf(compareTo);
-            case ">+":
+            case ">=":
                 return Double.valueOf(placeholders) >= Double.valueOf(compareTo);
-            case "++":
-            case "+":
+            case "==":
+            case "=":
                 return Double.valueOf(placeholders) == Double.valueOf(compareTo);
-            case "!+":
+            case "!=":
                 return Double.valueOf(placeholders) != Double.valueOf(compareTo);
             case "equals":
                 return placeholders.equals(compareTo);

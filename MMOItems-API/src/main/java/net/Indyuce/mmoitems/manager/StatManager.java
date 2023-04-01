4f8577736b9956842223865f1de6a3e4f015bc90
Jules <cym.peyrat@yahoo.fr>
2023-04-01 23:03:36 +0200

    Fixed lang3 import

--- MMOItems-API/src/main/java/net/Indyuce/mmoitems/manager/StatManager.java
+++ MMOItems-API/src/main/java/net/Indyuce/mmoitems/manager/StatManager.java
@@ -8,7 +8,7 @@ import net.Indyuce.mmoitems.api.ConfigFile;
 import net.Indyuce.mmoitems.api.Type;
 import net.Indyuce.mmoitems.stat.type.*;
 import net.Indyuce.mmoitems.util.ElementStatType;
-import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;

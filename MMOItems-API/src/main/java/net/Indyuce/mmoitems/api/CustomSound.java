559462cbcc2a5ff48f4f50c9960f7d65066cbc27
INSide_734 <3217962725@qq.com>
2024-06-21 19:49:42 +0800

    🐛 Fix bug

--- MMOItems-API/src/main/java/net/Indyuce/mmoitems/api/CustomSound.java
+++ MMOItems-API/src/main/java/net/Indyuce/mmoitems/api/CustomSound.java
@@ -10,7 +10,7 @@ public enum CustomSound {
     ON_BLOCK_BREAK(Material.COBBLESTONE, 25, "当方块被物品破坏时播放。"),
     ON_PICKUP(Material.IRON_INGOT, 28, "当从地上捡起物品时播放。"),
     ON_LEFT_CLICK(Material.STONE_AXE, 31, "左键单击项目时播放。"),
-    ON_CRAFT(VersionMaterial.CRAFTING_TABLE.toMaterial(), 34, "在制作物品栏中制作物品时播放", "或者在熔炉中熔炼成功时播放"),
+    ON_CRAFT(Material.CRAFTING_TABLE, 34, "在制作物品栏中制作物品时播放", "或者在熔炉中熔炼成功时播放"),
     ON_CONSUME(Material.APPLE, 37, "物品被消耗后播放。", "（吃/喝后动画）"),
     ON_ITEM_BREAK(Material.FLINT, 40, "当物品损坏时播放。"),
     ON_CROSSBOW(Material.ARROW, 38, "当弩射出箭时播放。"),
